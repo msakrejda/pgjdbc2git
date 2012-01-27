a37cf2abf078abd5005e20d3715be9ce87030bf0
Maciek Sakrejda <m.sakrejda@gmail.com>
2012-01-26 23:37:28 -0800

    Cleanup.

--- convert.sh
+++ convert.sh
@@ -2,26 +2,30 @@
 
 set -e
 
-tar_file=$1
+tar_file="$1"
 
 if [ ! -f "$tar_file" ]
 then
-	echo "Expected path to PostgreSQL JDBC CVS export; got '$tar_file'"
-	exit 1
+    echo "Expected path to PostgreSQL JDBC CVS export as argument; got '$tar_file'"
+    exit 1
 fi
 
 if [ -z "$CVS2GIT_HOME" ]
 then
     echo "Expected CVS2GIT_HOME environment variable pointing to root of cvs2git code checkout."
     echo "The current release version of cvs2git does not work with our configuration file."
-	echo "The trunk version can be checked out via"
-	echo 'svn co --username=guest --password="" http://cvs2svn.tigris.org/svn/cvs2svn/trunk cvs2svn-trunk'
+    echo "The trunk version can be checked out via"
+    echo 'svn co --username=guest --password="" http://cvs2svn.tigris.org/svn/cvs2svn/trunk cvs2svn-trunk'
     exit 1
 fi
 
+
 # Clean up and extract a fresh copy from the CVS repository archive
-rm -rf CVSROOT pgjdbc.git pgjdbc-checkout cvs2svn-tmp
-tar -zxf "$tar_file"
+rm -rf cvs-repo pgjdbc.git pgjdbc-checkout cvs2svn-tmp
+
+mkdir -p cvs-repo/CVSROOT
+pushd cvs-repo
+tar -zxf "../${tar_file}"
 
 # Relabel tag REL7_1 as REL7_1_BETA in various files that were deleted between
 # the original placement of that tag and its renaming.  The renaming action
@@ -115,40 +119,31 @@ rcs -x,v -nREL7_1 pgjdbc/postgresql/util/Attic/UnixCrypt.java
 # NOTE: the log messages for the dead revisions must match a regexp
 # inside cvs2git, or it won't do what we want with these.
 
-for patch in $PWD/patches/*.patch
+pushd pgjdbc
+for patch in ../../patches/*.patch
 do
     patched_files="$(grep -a ^diff $patch | awk '{ print $NF }')"
-    pushd pgjdbc
     chmod u+w $patched_files
     patch -p0 < $patch
     chmod u-w $patched_files
-	popd
 done
+popd
 
-mkdir CVSROOT
-mv pgjdbc CVSROOT
+popd
 
 $CVS2GIT_HOME/cvs2git --options=c2g.config.py --encoding=iso-8859-15
 
 mkdir pgjdbc.git
-cd pgjdbc.git
+pushd pgjdbc.git
 git init --bare
 cat ../cvs2svn-tmp/git-blob.dat ../cvs2svn-tmp/git-dump.dat | git fast-import
-
-cd ..
+popd
 
 git clone pgjdbc.git pgjdbc-checkout
 
-# Fix up the REL6_4 branch, which cvs2git puts in the wrong place (it's easier
-# to fix here than in CVS):
+# Fix up the REL6_4 branch, which cvs2git puts in the wrong place (it's easier to fix here than in CVS):
 cd pgjdbc-checkout
 git checkout -b REL6_4 $(git log --format='%H' --grep 'Remove various files that were moved to various subdirectories...')
 git push --force origin REL6_4
 git checkout master
 git branch -d REL6_4
-
-# Something like the following can be used to change the IDENTIFIER tags that
-# JDBC uses into just the file names (as the PostgreSQL core project did)
-#
-# find -type f | xargs sed -i -r 's/\$PostgreSQL: pgjdbc\/(.*),v .*\$\s*$/\1/'
-#
